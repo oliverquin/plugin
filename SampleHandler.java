f6aa9e65151bd7a1154400d95a459f4124429fa1
oliverquin <31702635+oliverquin@users.noreply.github.com>
2018-12-25 14:13:35 +0530

    JsOptimizeMode Plugin code commit

---
+++ SampleHandler.java
@@ -0,0 +1,20 @@
+package jsoptimizemode.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class SampleHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		Shell s = window.getShell();
+		MyDialog mydialog = new MyDialog(s);
+		mydialog.open();
+		return null;
+	}
+}
